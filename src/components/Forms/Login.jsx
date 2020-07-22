0f44e386d67eb9ca7371c2236c9f155411775310
Miftah Faris <miftah.faris07@yahoo.co.id>
2020-07-22 18:13:11 +0700

    syncronize login and service

--- src/components/Forms/Login.jsx
+++ src/components/Forms/Login.jsx
@@ -4,6 +4,8 @@ import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import { useDispatch } from "react-redux";
 import { login } from "../../redux/actions";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
     form: {
@@ -13,51 +15,81 @@ const useStyles = makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
+    error: {
+        color: "red",
+        fontStyle: "italic",
+    },
 }));
+
+const CustomField = (props) => {
+    return (
+        <TextField
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            required
+            label="Email Address"
+            autoComplete="email"
+            autoFocus
+            {...props}
+        />
+    );
+};
 
 export default function Login() {
     const classes = useStyles();
     const dispatch = useDispatch();
+    const history = useHistory();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        dispatch(login());
-    };
     return (
-        <form onSubmit={handleSubmit} className={classes.form} noValidate>
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-            />
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-            />
+        <Formik
+            initialValues={{ email: "", password: "" }}
+            validate={(values) => {
+                const errors = {};
+                if (!values.email) {
+                    errors.email = "Required";
+                } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                        values.email
+                    )
+                ) {
+                    errors.email = "Invalid email address";
+                }
 
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-            >
-                Sign In
-            </Button>
-        </form>
+                if (!values.password) {
+                    errors.password = "Required";
+                }
+                return errors;
+            }}
+            onSubmit={(values) => {
+                dispatch(login(values, history));
+            }}
+        >
+            {({ isSubmitting }) => (
+                <Form className={classes.form}>
+                    <Field type="email" as={CustomField} name="email" />
+                    <ErrorMessage
+                        name="email"
+                        component="div"
+                        className={classes.error}
+                    />
+                    <Field type="password" as={CustomField} name="password" />
+                    <ErrorMessage
+                        name="password"
+                        component="div"
+                        className={classes.error}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign In
+                    </Button>
+                </Form>
+            )}
+        </Formik>
     );
 }
