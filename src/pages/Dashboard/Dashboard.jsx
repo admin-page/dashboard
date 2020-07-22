41d5f97473569049888386e65e7bed5ad759fbde
Miftah Faris <miftah.faris07@yahoo.co.id>
2020-07-22 14:44:47 +0700

    add login page and delete unnecessary component

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,29 +3,15 @@ import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Drawer from "@material-ui/core/Drawer";
-import Box from "@material-ui/core/Box";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
 import Container from "@material-ui/core/Container";
 
-import Link from "@material-ui/core/Link";
-import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import NotificationsIcon from "@material-ui/icons/Notifications";
 import { ListMenuItem } from "../../components";
 import { Switch, Route } from "react-router-dom";
-import {
-    Admins,
-    Users,
-    Houses,
-    Home,
-    Dashboard as DashboardPage,
-} from "../../pages";
+import { Admins, Users, Houses, Dashboard as DashboardPage } from "../../pages";
 
 const drawerWidth = 240;
 
@@ -111,13 +97,9 @@ const useStyles = makeStyles((theme) => ({
 export default function Dashboard() {
     const classes = useStyles();
     const [open, setOpen] = React.useState(true);
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
     const handleDrawerClose = () => {
         setOpen(false);
     };
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
     return (
         <div className={classes.root}>
@@ -145,9 +127,6 @@ export default function Dashboard() {
                 <div className={classes.appBarSpacer} />
                 <Container maxWidth="lg" className={classes.container}>
                     <Switch>
-                        <Route exact path="/">
-                            <Home />
-                        </Route>
                         <Route exact path="/dashboard">
                             <DashboardPage />
                         </Route>
